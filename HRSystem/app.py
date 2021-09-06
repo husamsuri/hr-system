ad71fe77086344bf26cc2e31b0031a3a6ca912b1
husamsuri <husamalsuri@gmail.com>
2021-09-06 20:30:30 +0300

    bug fix: create Resumes dir if it doesn't exist

--- HRSystem/app.py
+++ HRSystem/app.py
@@ -12,6 +12,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in allowed_files
 
+
 @app.route('/apply', methods=['POST'])
 def apply():
     try:
@@ -43,20 +44,25 @@ def apply():
     try:
         int(request.form['ExperienceYears'])
     except ValueError:
-       return {
-           "error": "ExperienceYears should be number"
-       }, 400
+        return {
+            "error": "ExperienceYears should be number"
+        }, 400
 
     try:
         datetime.fromisoformat(request.form['birthDate'])
     except ValueError:
-       return {
-           "error": "not a valid date"
-       }, 400
+        return {
+            "error": "not a valid date"
+        }, 400
 
     cur.execute(
         f"INSERT INTO applicants (name, DOB, experience_years, department, created_at) VALUES ('{request.form ['name']}', '{request.form['birthDate']}', {request.form['ExperienceYears']}, '{department}', '{datetime.now()}');")
     conn.commit()
+
+    is_dir = os.path.exists(UPLOAD_FOLDER)
+    if (not is_dir):
+        os.mkdir(UPLOAD_FOLDER)
+
     file.save(os.path.join(
         UPLOAD_FOLDER, f"{cur.lastrowid}.{file.filename.rsplit('.', 1)[1].lower()}"))
 
@@ -80,9 +86,10 @@ def list():
     rv = cur.fetchall()
     json_data = []
     for result in rv:
-        json_data.append(dict(zip(row_headers,result)))
+        json_data.append(dict(zip(row_headers, result)))
 
     return {"data": json_data}
+
 
 @app.route('/file/<id>')
 def file(id):
@@ -100,6 +107,7 @@ def file(id):
             return {
                 "error": "file not found"
             }, 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
